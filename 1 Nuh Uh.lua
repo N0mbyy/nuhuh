6548955eb7a14680c8b4f77f73630d3aa2adfaa0
MilfHunter69 <n0mbyy@protonmail.com>
2023-12-27 14:06:42 +0100

    1.17     1.17

--- 1 Nuh Uh.lua
+++ 1 Nuh Uh.lua
@@ -1,9 +1,16 @@
 local func = require("NovaScript.functions")
 local scripts_dir = filesystem.scripts_dir()
 local scriptName = "Stand Expansion"
-local myVersion = 1.16
+local myVersion = 1.17
 local response = false
 local toast = util.toast
+local log_dir = filesystem.stand_dir() .. '\\Log.txt'
+local full_stdout = ""
+local disp_stdout = ""
+local max_chars = 200
+local max_lines = 20
+local font_size = 0.35
+local timestamp_toggle = false
 require("lua_imGUI V2")
 require("Universal_ped_list")
 require("Universal_objects_list")
@@ -5167,6 +5174,97 @@ end)
 -----------------------------------------------------------------------------------------------------------------------------------
 
 local debugFeats = menu.list(menuroot, "Debug", {}, "")
+
+local konsole = menu.list(debugfeats, "Console")
+
+local function get_stand_stdout(tbl, n)
+    local all_lines = {}
+    local disp_lines = {}
+    local size = #tbl
+    local index = 1
+    if size >= n then 
+        index = #tbl - n
+    end
+
+    for i=index, size do 
+        local line = tbl[i]
+        local line_copy = line
+        if line ~= "" and line ~= '\n' then
+            all_lines[#all_lines + 1] = line
+            if not timestamp_toggle then
+                local _, second_segment = string.partition(line, ']')
+                if second_segment ~= nil then
+                    line = second_segment
+                end
+            end
+            if string.len(line) > max_chars then
+                disp_lines[#disp_lines + 1] = line:sub(1, max_chars) .. ' ...'
+            else
+                disp_lines[#disp_lines + 1] = line
+            end
+        end
+    end
+
+    full_stdout = table.concat(all_lines, '\n')
+    disp_stdout = table.concat(disp_lines, '\n')
+end
+
+local function get_last_lines(file)
+    local f = io.open(file, "r")
+    local len = f:seek("end")
+    f:seek("set", len - max_lines*1000)
+    local text = f:read("*a")
+    lines = string.split(text, '\n')
+    f:close()
+    get_stand_stdout(lines, max_lines)
+end
+
+menu.action(konsole, "Copy STDOUT to clipboard", {}, "Copy the full, untrimmed last x lines of the STDOUT to clipboard.", function()
+    util.copy_to_clipboard(full_stdout, true)
+end)
+
+
+menu.slider(konsole, "Max display chars", {"nconsolemaxchars"}, "", 1, 1000, 200, 1, function(s)
+    max_chars = s
+end)
+
+menu.slider(konsole, "Max display lines", {"nconsolemaxlines"}, "", 1, 60, 20, 1, function(s)
+    max_lines = s
+end)
+
+menu.slider_float(konsole, "Font size", {"nconsolemaxlines"}, "", 1, 1000, 35, 1, function(s)
+    font_size = s*0.01
+end)
+
+menu.toggle(konsole, "Show timestamps", {"ndrawconsole"}, "", function(on)
+    timestamp_toggle = on
+end, false)
+
+draw_toggle = false
+menu.toggle(konsole, "Draw console", {"ndrawconsole"}, "", function(on)
+    draw_toggle = on
+end, false)
+
+local text_color = {r = 1, g = 1, b = 1, a = 1}
+menu.colour(konsole, "Text color", {"nconsoletextcolor"}, "", 1, 1, 1, 1, true, function(on_change)
+    text_color = on_change
+end)
+
+local bg_color = {r = 0, g = 0, b = 0, a = 0.75}
+menu.colour(konsole, "BG color", {"nconsolebgcolor"}, "", 0, 0, 0, 0.5, true, function(on_change)
+    bg_color = on_change
+end)
+
+util.create_tick_handler(function()
+    local text = get_last_lines(log_dir)
+    if draw_toggle and menu.is_open() then
+        local size_x, size_y = directx.get_text_size(disp_stdout, font_size)
+        size_x += 0.01
+        size_y += 0.01
+        directx.draw_rect(0.0, 0.25, size_x, size_y, bg_color)
+        directx.draw_text(0.0, 0.255, disp_stdout, 0, font_size, text_color, true)
+    end
+end)
 
 menuAction(debugFeats, "Get V3 Coords", {"printcoords"}, "Toasts your coordinates.", function()
     local playerCoords = getEntityCoords(getPlayerPed(players.user()), true)
